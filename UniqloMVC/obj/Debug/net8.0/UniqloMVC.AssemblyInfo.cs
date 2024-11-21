bfb4cbc05409dd688e4b018c6233ad3b0542102a
xaqan05 <xaqan2005@gmail.com>
2024-11-21 04:30:33 +0400

    version 1.1

--- UniqloMVC/obj/Debug/net8.0/UniqloMVC.AssemblyInfo.cs
+++ UniqloMVC/obj/Debug/net8.0/UniqloMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniqloMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8d5f147fec48d0cc1a07fe43fe1805d01e3b91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adabff6bfcc50009b78e24dd2ce07c45a97fe04b")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniqloMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniqloMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
